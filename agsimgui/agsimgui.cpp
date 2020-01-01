a8a696d8f416609964f5049611a4bd20942cbdba
ericoporto <ericoporto2008@gmail.com>
2020-01-01 14:49:42 -0300

    adds DragInt and DragFloat controls

--- agsimgui/agsimgui.cpp
+++ agsimgui/agsimgui.cpp
@@ -377,6 +377,14 @@ namespace agsimgui {
 " /// bool selected carry the selection state (read-only). When Selectable() is clicked it returns true so you can modify your selection state. \r\n"
 " import static bool Selectable(String label, bool selected = false, ImGuiSelectableFlags flags = 0, int width = 0, int height = 0); \r\n"
 "  \r\n"
+" // Widgets: Drag \r\n"
+"  \r\n"
+" /// CTRL+Click on any drag box to turn them into an input box. Manually input values aren't clamped and can go off-bounds. \r\n"
+" import static float DragFloat(String label, float value, float min_value = 0, float max_value = 0, float speed = 0, String format = 0); \r\n"
+"  \r\n"
+" /// CTRL+Click on any drag box to turn them into an input box. Manually input values aren't clamped and can go off-bounds. \r\n"
+" import static int DragInt(String label, int value, int min_value = 0, int max_value = 0, float speed = 0, String format = 0); \r\n"
+"  \r\n"
 " // Widgets: Combobox commands \r\n"
 "  \r\n"
 " /// The BeginCombo()/EndCombo() allows to manage your contents and selection state however you want it, by creating e.g. Selectable() items. \r\n"
@@ -673,6 +681,34 @@ void AgsImGui_EndCombo(){
     ImGui::EndCombo();
 }
 
+uint32_t AgsImGui_DragFloat(const char* label, uint32_t value, uint32_t v_min, uint32_t v_max, uint32_t speed, const char* format){
+    std::string format_string =  "%.3f";
+    std::string empty_string =  "";
+    float f_value = ToNormalFloat(value);
+    float f_speed = ToNormalFloat(speed);
+    float f_v_min = ToNormalFloat(v_min);
+    float f_v_max = ToNormalFloat(v_max);
+    if(f_speed == 0.0) f_speed = 1.0;
+    if(format == nullptr) format = format_string.c_str();
+    if(label == nullptr) label = empty_string.c_str();
+
+    ImGui::DragFloat(label, &f_value, f_speed, f_v_min, f_v_max, format);
+    return ToAgsFloat(f_value);
+}
+
+int AgsImgui_DragInt(const char* label, int value, int v_min, int v_max, uint32_t speed, const char* format){
+    std::string format_string =  "%d";
+    std::string empty_string =  "";
+    float f_speed = ToNormalFloat(speed);
+    if(f_speed == 0.0) f_speed = 1.0;
+    if(format == nullptr) format = format_string.c_str();
+    if(label == nullptr) label = empty_string.c_str();
+
+    int ret_value = value;
+    ImGui::DragInt(label, &ret_value, f_speed, v_min, v_max, format);
+    return ret_value;
+}
+
 bool AgsImGui_BeginListBox(const char* name, int items_count, int height_in_items = -1){
     return ImGui::ListBoxHeader(name,items_count,height_in_items);
 }
@@ -830,6 +866,8 @@ void AgsImGui_ValueFloat(const char* prefix, uint32_t value){
         engine->RegisterScriptFunction("AgsImGui::RadioButton^2", (void*)AgsImGui_RadioButton);
         engine->RegisterScriptFunction("AgsImGui::Bullet^0", (void*)AgsImGui_Bullet);
         engine->RegisterScriptFunction("AgsImGui::Selectable^5", (void*)AgsImGui_Selectable);
+        engine->RegisterScriptFunction("AgsImGui::DragFloat^6", (void*)AgsImGui_DragFloat);
+        engine->RegisterScriptFunction("AgsImGui::DragInt^6", (void*)AgsImgui_DragInt);
         engine->RegisterScriptFunction("AgsImGui::BeginCombo^3", (void*)AgsImGui_BeginCombo);
         engine->RegisterScriptFunction("AgsImGui::EndCombo^0", (void*)AgsImGui_EndCombo);
         engine->RegisterScriptFunction("AgsImGui::BeginListBox^3", (void*)AgsImGui_BeginListBox);
