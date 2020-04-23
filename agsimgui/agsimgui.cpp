ab7681a8ce7c3078233738b98b7e3f57017426f3
ericoporto <ericoporto2008@gmail.com>
2020-04-23 19:30:06 -0300

    advjam 2020 temp theme until I figure how to expose this

--- agsimgui/agsimgui.cpp
+++ agsimgui/agsimgui.cpp
@@ -800,7 +800,7 @@ namespace agsimgui {
 		// User has chosen to view the Properties of the plugin
 		// We could load up an options dialog or something here instead
 /*	MessageBox(parent,
-			 L"agsimgui v0.2.0 By eri0o",
+			 L"agsimgui v0.2.advjam By eri0o",
 			 L"About",
 		 MB_OK | MB_ICONINFORMATION);
  */
@@ -1607,7 +1607,51 @@ int AgsImGuiHelper_GetClipboarImage() {
         style.WindowRounding = 0.0f;
         style.Alpha = 1.0f;
 
-        ImGui::StyleColorsDark();
+
+        ImGui::StyleColorsLight();
+
+        ImVec4 white = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+        ImVec4 transparent = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+        ImVec4 dark = ImVec4(0.00f, 0.00f, 0.00f, 0.20f);
+        ImVec4 darker = ImVec4(0.00f, 0.00f, 0.00f, 0.50f);
+
+        ImVec4 background = ImVec4(0.95f, 0.95f, 0.95f, 1.00f);
+        ImVec4 text = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+        ImVec4 border = ImVec4(0.64f, 0.60f, 0.60f, 1.00f);
+        ImVec4 grab = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
+        ImVec4 header = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+        ImVec4 active = ImVec4(0.90f, 0.47f, 0.64f, 1.00f);
+        ImVec4 hover = ImVec4(0.40f, 0.87f, 0.84f, 0.20f);
+
+        style.Colors[ImGuiCol_Text] = text;
+        style.Colors[ImGuiCol_WindowBg] = background;
+        style.Colors[ImGuiCol_ChildBg] = background;
+        style.Colors[ImGuiCol_PopupBg] = white;
+
+        style.Colors[ImGuiCol_Border] = border;
+        style.Colors[ImGuiCol_BorderShadow] = transparent;
+
+        style.Colors[ImGuiCol_Button] = header;
+        style.Colors[ImGuiCol_ButtonHovered] = hover;
+        style.Colors[ImGuiCol_ButtonActive] = active;
+
+        style.Colors[ImGuiCol_FrameBg] = white;
+        style.Colors[ImGuiCol_FrameBgHovered] = hover;
+        style.Colors[ImGuiCol_FrameBgActive] = active;
+
+        style.Colors[ImGuiCol_MenuBarBg] = header;
+        style.Colors[ImGuiCol_Header] = header;
+        style.Colors[ImGuiCol_HeaderHovered] = hover;
+        style.Colors[ImGuiCol_HeaderActive] = active;
+
+        style.Colors[ImGuiCol_CheckMark] = text;
+        style.Colors[ImGuiCol_SliderGrab] = grab;
+        style.Colors[ImGuiCol_SliderGrabActive] = darker;
+
+        style.Colors[ImGuiCol_ScrollbarBg] = header;
+        style.Colors[ImGuiCol_ScrollbarGrab] = grab;
+        style.Colors[ImGuiCol_ScrollbarGrabHovered] = dark;
+        style.Colors[ImGuiCol_ScrollbarGrabActive] = darker;
 
         ImGuiIO& io = ImGui::GetIO();
         io.Fonts->Flags |= ImFontAtlasFlags_NoPowerOfTwoHeight | ImFontAtlasFlags_NoMouseCursors;
@@ -1826,7 +1870,7 @@ enum MouseButton {
 			//initialize debug
 			if (!screen.initialized) {
 				engine->GetScreenDimensions(&screen.width, &screen.height, &screen.colorDepth);
-				printf("\nagsimgui 0.2.0\n");
+				printf("\nagsimgui 0.2.advjam\n");
 
 				if (screen.driver == Screen::Driver::eOpenGL) {
 
